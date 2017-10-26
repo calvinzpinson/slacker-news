ebe5f11c9e00d7f87ae877807483f45e39afd27d
Calvin <calvinzpinson@gmail.com>
2017-10-26 10:48:14 -0700

    more login work

--- src/main/java/com/slackernews/controller/LoginController.java
+++ src/main/java/com/slackernews/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.slackernews.controller;
 
+import com.slackernews.model.Validator.UserCreationFormValidator;
+import com.slackernews.service.Implementation.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +15,20 @@ import java.util.Optional;
 
 @Controller
 public class LoginController {
+
+    private final UserService userService;
+    private final UserCreationFormValidator userCreateFormValidator;
+
+    @Autowired
+    public LoginController(UserService userService, UserCreationFormValidator userCreationFormValidator) {
+        this.userService = userService;
+        this.userCreateFormValidator = userCreationFormValidator;
+    }
+
+    @InitBinder("form")
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(userCreateFormValidator);
+    }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView getLoginPage(@RequestParam Optional<String> error) {

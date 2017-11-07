fdce58fbad9dfdc5c6501fae4e07e96a3574bdfa
Calvin <calvinzpinson@gmail.com>
2017-11-06 18:04:30 -0800

    added skeleton for post specific page

--- src/main/java/com/slackernews/controller/PostController.java
+++ src/main/java/com/slackernews/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.slackernews.controller;
 
 import com.slackernews.model.CurrentUser;
+import com.slackernews.model.Post;
 import com.slackernews.model.PostCreationForm;
 import com.slackernews.model.User;
 import com.slackernews.service.Interface.IPostService;
@@ -9,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.validation.Valid;
 import java.net.MalformedURLException;
 import java.security.Principal;
+import java.util.NoSuchElementException;
 
 @Controller
 public class PostController {
@@ -46,5 +49,11 @@ public class PostController {
             return "submit";
         }
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/pos/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostPage(@PathVariable int id) {
+        return new ModelAndView("postView", "post", postService.getPostById(id)
+                .orElseThrow(() -> new NoSuchElementException("Post not found")));
     }
 }
